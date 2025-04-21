288d6b23ea618aab6e68f1491a7e0d12435d610c
chimallidev <chimalli.dev@gmail.com>
2025-04-21 16:26:51 -0600

    index.html configurado en la funcion home y fichero utils.py creado

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status 
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import utils
 
 app= FastAPI()
 
@@ -10,6 +12,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 #Configura Jinja2
 templates= Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def home():
-    return "Hola Mundo"
+@app.get("/", response_class = HTMLResponse, status_code = status.HTTP_200_OK)
+async def home(request: Request):
+    context = {
+        "request": request,
+        "pagina_titulo": utils.index_title,
+        "meta_descripcion": utils.index_meta_description
+    }
+
+    try:
+        return templates.TemplateResponse("index.html", context)
+    
+    except:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail = "La página no ha sido encontrada")
---
+++ utils.py
@@ -0,0 +1,6 @@
+#Titulo y meta descripción de la pagina INDICE
+index_title = "👨🏽‍💻Chimallidev | 👾Desarrollador especializado en Python"
+
+index_meta_description = """Desarrollador autodidacta y freelance, 
+        especializado en Python con conocimientos en Kotlin. 
+        En este espacio encontrarás todos mis enlaces relevantes. ¡Bienvenid@!🚀"""
